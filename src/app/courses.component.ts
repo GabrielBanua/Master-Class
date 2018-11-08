bb97bfff66104fac614bf0599e7563f0e40cf24e
GabrielBanua <banua.gabrielle10@gmail.com>
2018-11-08 20:50:56 +0800

    Property Binding example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
     selector: 'courses',
     template: `
         <h2>{{ title }}</h2>
-        `
+        <h2 [textContent]="title"></h2>
+
+        <img src="{{ imageUrl }}" />
+        <img [src]="imageUrl" /> 
+        `// Property binding example <img [src]="imageUrl"/> && <h2 [textContent]="title"></h2>
 })
 export class CoursesComponent{
    title = "List of courses";
+   imageUrl = "http://lorempixel.com/400/200";
 }
