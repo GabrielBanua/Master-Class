987b8fa8d7731743940203d66769878b06e84de3
GabrielBanua <banua.gabrielle10@gmail.com>
2018-11-08 22:00:26 +0800

    Adding Bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,18 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <img [src]="imageUrl" /> 
-        <table>
-            <tr>
-                <td [attr.colspan]="colSpan"></td>
-            </tr>
-        </table>
-        `
-        //Attribute binding example <td [attr.colspan]="colSpan"></td>
-        /**note DOM element don't have an attribute/property like colspan in HTML thats why we need to tell angular when 
-          binding colspan in DOM element that this is an HTML attribute thats why we add "attr.colspan"**/
+        <button class="btn btn-primary">Save</button>    
+    `
+    /**Adding or installing bootstrap in angular by typing the command in the terminal "npm install bootstrap --save" 
+    and importing it to styles.css by the following syntax "@import "~bootstrap/dist/css/bootstrap.css"" to be used */
 })
 export class CoursesComponent{
-   imageUrl = "";
-   colSpan = 2;
+  
 }
