b5fbc19269d07b5160df3e1ceaeac51773f0b27e
GabrielBanua <banua.gabrielle10@gmail.com>
2018-11-08 22:08:28 +0800

    Class Binding example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary">Save</button>    
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>    
     `
-    /**Adding or installing bootstrap in angular by typing the command in the terminal "npm install bootstrap --save" 
-    and importing it to styles.css by the following syntax "@import "~bootstrap/dist/css/bootstrap.css"" to be used */
+    //Example of Class binding <button [class.active]="isActive">Save</button>
 })
 export class CoursesComponent{
-  
+  isActive = false;
 }
